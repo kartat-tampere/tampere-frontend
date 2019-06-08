3c85e4a916ac44c2bf2ad2c591bffbc9dcab3d2d
Sami Mäkinen <zakarfin@gmail.com>
2019-06-08 16:46:36 +0300

    Styling

--- bundles/file-upload/components/FileInput.jsx
+++ bundles/file-upload/components/FileInput.jsx
@@ -5,50 +5,64 @@ import styled from "styled-components";
 const StyledRootEl = styled("div")`
   margin: 10px;
   padding: 10px;
-  border: 1px solid black;
+  border: 1px dotted black;
 `;
-function handleFiles(fileList) {
-  if (!fileList) {
-    return false;
-  }
-  fileList.forEach(element => {
-    console.log(element);
-  });
-}
-function onChange(event) {
-  handleFiles(event.target.files);
-}
 
-function onDrop(event) {
-  event.preventDefault();
-  handleFiles(event.dataTransfer.files);
-}
+const StyledFileInput = styled("input")`
+  display: none;
+`;
 
-function onDragOver(event) {
-  event.preventDefault();
-  console.log("Drag over", event);
-}
-function onDragLeave(event) {
-  event.preventDefault();
-  console.log("Drag leave", event);
-}
-function onDragEnter(event) {
-  event.preventDefault();
-  console.log("Drag enter", event);
-}
+const ButtonLabel = styled("label")`
+  display: block;
+  text-align: center;
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px;
+  background-color: ghostwhite;
+`;
 
-export const FileInput = () => {
+export const FileInput = ({ onFiles }) => {
+  const onDrop = event => {
+    event.preventDefault();
+    onFiles(event.dataTransfer.files);
+  };
   return (
     <StyledRootEl
-      class="dropzone"
+      className="dropzone"
       onDragEnter={onDragEnter}
       onDragOver={onDragOver}
       onDrop={onDrop}
       onDragLeave={onDragLeave}
     >
-      <input type="file" multiple onChange={onChange} />
+      <ButtonLabel>
+        <StyledFileInput
+          type="file"
+          multiple
+          onChange={e => onFiles(e.target.files)}
+        />
+        Valitse tiedostoja tai pudota ne tähän
+      </ButtonLabel>
     </StyledRootEl>
   );
 };
 
-FileInput.propTypes = {};
+FileInput.propTypes = {
+  onFiles: PropTypes.func
+};
+
+/* -------------------------
+ * Just for debugging
+ */
+function onDragOver(event) {
+  event.preventDefault();
+  //console.log("Drag over", event);
+}
+function onDragLeave(event) {
+  event.preventDefault();
+  //console.log("Drag leave", event);
+}
+function onDragEnter(event) {
+  event.preventDefault();
+  //console.log("Drag enter", event);
+}
--- bundles/file-upload/components/FileUploadPanel.jsx
+++ bundles/file-upload/components/FileUploadPanel.jsx
@@ -4,12 +4,21 @@ import PropTypes from "prop-types";
 import { LayerDetails } from "./LayerDetails";
 import { FileInput } from "./FileInput";
 
+function handleFiles(fileList) {
+  if (!fileList) {
+    return false;
+  }
+  fileList.forEach(element => {
+    console.log(element);
+  });
+}
+
 export const FileUploadPanel = ({ layer, onSubmit, ...other }) => {
   return (
     <div>
       <LayerDetails {...layer} />
       <form onSubmit={onSubmit}>
-        <FileInput />
+        <FileInput onFiles={handleFiles} />
         <input type="submit" />
       </form>
     </div>
