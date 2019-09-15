98ce95fde096a876cfb778f9a670b2c6326c8b98
Sami Mäkinen <zakarfin@gmail.com>
2019-09-15 14:42:41 +0300

    ESLint fixes

--- bundles/file-upload/components/FileUploadPanel.stories.js
+++ bundles/file-upload/components/FileUploadPanel.stories.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { storiesOf } from "@storybook/react";
-import { FileUploadPanel } from "./FileUploadPanel";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { FileUploadPanel } from './FileUploadPanel';
 
 const layer = {
-  id: 1,
-  name: "Tason nimi"
+    id: 1,
+    name: 'Tason nimi'
 };
-function onSubmit(data) {
-  alert(
-    `Attr: ${data.layerAttribute} \nTiedostot: ${data.files
-      .map(f => f.name)
-      .join(", ")}`
-  );
+function onSubmit (data) {
+    alert(
+        `Attr: ${data.layerAttribute} \nTiedostot: ${data.files
+            .map(f => f.name)
+            .join(', ')}`
+    );
 }
-storiesOf("FileUploadPanel", module).add("basic", () => (
-  <FileUploadPanel layer={layer} onSubmit={onSubmit} />
+storiesOf('FileUploadPanel', module).add('basic', () => (
+    <FileUploadPanel layer={layer} onSubmit={onSubmit} />
 ));
--- bundles/file-upload/instance.js
+++ bundles/file-upload/instance.js
@@ -1,81 +1,80 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { FileUploadPanel } from "./components/FileUploadPanel";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FileUploadPanel } from './components/FileUploadPanel';
 
-const BasicBundle = Oskari.clazz.get("Oskari.BasicBundle");
+const BasicBundle = Oskari.clazz.get('Oskari.BasicBundle');
 
 Oskari.clazz.defineES(
-  "Oskari.file-upload.BundleInstance",
-  class FileUploadBundle extends BasicBundle {
-    constructor() {
-      super();
-      this.__name = "file-upload";
-    }
-    _startImpl() {
-      const root = document.getElementById("loginbar");
-      if (!root) {
-        alert("prob");
-      }
+    'Oskari.file-upload.BundleInstance',
+    class FileUploadBundle extends BasicBundle {
+        constructor () {
+            super();
+            this.__name = 'file-upload';
+        }
+        _startImpl () {
+            const root = document.getElementById('loginbar');
+            if (!root) {
+                alert('prob');
+            }
 
-      var flyout = Oskari.clazz.create(
-        "Oskari.userinterface.extension.ExtraFlyout",
-        "File upload"
-      );
-      flyout.show();
-      flyout.move(170, 0, true);
-      flyout.makeDraggable();
+            var flyout = Oskari.clazz.create(
+                'Oskari.userinterface.extension.ExtraFlyout',
+                'File upload'
+            );
+            flyout.show();
+            flyout.move(170, 0, true);
+            flyout.makeDraggable();
 
-      var mainUI = jQuery("<div></div>");
-      ReactDOM.render(
+            var mainUI = jQuery('<div></div>');
+            ReactDOM.render(
         <>
           <FileUploadPanel onSubmit={submitFiles} />
         </>,
         mainUI[0]
-      );
-      flyout.setContent(mainUI);
+            );
+            flyout.setContent(mainUI);
+        }
     }
-  }
 );
 
-function submitFiles(data) {
-  /*
+function submitFiles (data) {
+    /*
   alert(
     `Attr: ${data.layerAttribute} \nTiedostot: ${data.files
       .map(f => f.name)
       .join(", ")}`
-  );*/
-  uploadFile(data.files); //.forEach(uploadFile);
+  ); */
+    uploadFile(data.files); // .forEach(uploadFile);
 }
 
-function updateProgress(file, progress) {
-  console.log("Progress on " + file.name + ": " + progress);
+function updateProgress (file, progress) {
+    console.log('Progress on ' + file.name + ': ' + progress);
 }
 
-function uploadFile(files) {
-  var url = Oskari.urls.getRoute("WFSAttachments");
-  var xhr = new XMLHttpRequest();
-  var formData = new FormData();
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("X-XSRF-TOKEN", Oskari.app.getXSRFToken());
+function uploadFile (files) {
+    var url = Oskari.urls.getRoute('WFSAttachments');
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('X-XSRF-TOKEN', Oskari.app.getXSRFToken());
 
-  // Add following event listener
-  xhr.upload.addEventListener("progress", function(e) {
-    updateProgress(files, (e.loaded * 100.0) / e.total || 100);
-  });
+    // Add following event listener
+    xhr.upload.addEventListener('progress', function (e) {
+        updateProgress(files, (e.loaded * 100.0) / e.total || 100);
+    });
 
-  xhr.addEventListener("readystatechange", function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      // Done. Inform the user
-    } else if (xhr.readyState == 4 && xhr.status != 200) {
-      // Error. Inform the user
-    }
-  });
+    xhr.addEventListener('readystatechange', function (e) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Done. Inform the user
+        } else if (xhr.readyState === 4 && xhr.status !== 200) {
+            // Error. Inform the user
+        }
+    });
 
-  formData.append("layerId", "1");
-  files.forEach(f => {
-    formData.append("file", f);
-    formData.append("locale_" + f.name, f.locale);
-    
-  });
-  xhr.send(formData);
+    formData.append('layerId', '1');
+    files.forEach(f => {
+        formData.append('file', f);
+        formData.append('locale_' + f.name, f.locale);
+    });
+    xhr.send(formData);
 }
