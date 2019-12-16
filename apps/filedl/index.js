061256f07a58ac7834d8428995814c627f917528
Sami Mäkinen <zakarfin@gmail.com>
2019-12-16 23:25:25 +0200

    Add filedl app

---
+++ apps/filedl/index.js
@@ -0,0 +1,32 @@
+'use strict';
+/**
+ * Start when dom ready
+ */
+jQuery(document).ready(function () {
+    var getAppSetupParams = {};
+    // populate url with possible control parameters
+    Object.keys(window.controlParams || {}).forEach(function (key) {
+        getAppSetupParams[key] = window.controlParams[key];
+    });
+
+    jQuery.ajax({
+        type: 'POST',
+        dataType: 'json',
+        data: getAppSetupParams,
+        url: '/oskari/action?action_route=GetAppSetup',
+        success: function (appSetup) {
+            var app = Oskari.app;
+            if (!appSetup.startupSequence) {
+                jQuery('#mapdiv').append('Unable to start');
+                return;
+            }
+            app.init(appSetup);
+            app.startApplication();
+        },
+        error: function (jqXHR, textStatus) {
+            if (jqXHR.status !== 0) {
+                jQuery('#mapdiv').append('Unable to start');
+            }
+        }
+    });
+});
---
+++ apps/filedl/main.js
@@ -0,0 +1,15 @@
+import 'oskari-loader!../../bundles/lang-overrides/bundle.js';
+
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapmodule/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapwmts/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/wfsvector/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/oskariui/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/mapfull/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/ui-components/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/toolbar/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/infobox/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/drawtools/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/maprotator/bundle.js';
+
+import 'oskari-loader!../../bundles/file-layerlist/bundle.js';
+import './css/overwritten.css';
