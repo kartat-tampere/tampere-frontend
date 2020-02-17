97195edda77466e3e86156d252938f3b3f4fecfc
Sami Mäkinen <zakarfin@gmail.com>
2020-02-17 23:55:58 +0200

    More order fixing

--- apps/embedded/main.js
+++ apps/embedded/main.js
@@ -1,13 +1,16 @@
 import 'oskari-loader!../../bundles/lang-overrides/bundle.js';
 
-import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapmodule/bundle.js';
+// These framework bundles have to be imported first
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/mapfull/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/oskariui/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/ui-components/bundle.js';
+
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapwmts/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/wfsvector/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/maparcgis/bundle.js';
 //import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapuserlayers/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/oskariui/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/mapfull/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/ui-components/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/toolbar/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/publishedstatehandler/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/divmanazer/bundle.js';
