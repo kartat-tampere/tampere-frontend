6f602d5cebe47f3fd2dcb0bb4e036cff407dab28
Sami Mäkinen <zakarfin@gmail.com>
2020-04-29 23:26:31 +0300

    Fix import order

--- apps/embedded/main.js
+++ apps/embedded/main.js
@@ -4,22 +4,24 @@ import 'oskari-loader!../../bundles/lang-overrides/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/mapfull/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/oskariui/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/ui-components/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/divmanazer/bundle.js';
 
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapmodule/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapwmts/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/wfsvector/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/maparcgis/bundle.js';
 //import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapuserlayers/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/oskariui/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/mapfull/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/mapping/ol/toolbar/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/publishedstatehandler/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/divmanazer/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/featuredata2/bundle.js';
+
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/infobox/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/drawtools/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/toolbar/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/publishedstatehandler/bundle.js';
+
 import 'oskari-loader!oskari-frontend/packages/framework/bundle/coordinatetool/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/rpc/bundle.js';
 import 'oskari-loader!oskari-frontend/packages/mapping/ol/maprotator/bundle.js';
-import 'oskari-loader!oskari-frontend/packages/framework/bundle/maplegend/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/rpc/bundle.js';
+
+import 'oskari-lazy-loader?maplegend!oskari-frontend/packages/framework/bundle/maplegend/bundle.js';
+import 'oskari-lazy-loader?featuredata2!oskari-frontend/packages/framework/featuredata2/bundle.js';
 
 import './css/overwritten.css';
