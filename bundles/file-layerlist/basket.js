4bd1f7728914597f58102717589b3b38f8a6de5f
Sami Mäkinen <zakarfin@gmail.com>
2020-05-21 23:36:42 +0300

    Make basket observable

--- bundles/file-layerlist/basket.js
+++ bundles/file-layerlist/basket.js
@@ -1,60 +1,32 @@
 import React from 'react';
 import { Messaging } from 'oskari-ui/util';
+import { getFeatureElement } from './featureshelper';
 
 const getFileId = (file) => {
     return file._$layerId + '||' + (file._oid || file.__fid);
 };
-const IGNORED_KEYS = ['_$layerId', '_oid', '__fid', '_$files'];
 export const showMessage = (file) => {
-    const shownKeys = Object.keys(file)
-        .filter(key => !IGNORED_KEYS.includes(key))
-        .map(key => (
-            <tr key={key}>
-                <td>{key}</td>
-                <td>{file[key]}</td>
-            </tr>));
-    // {"external":false,"layerId":2276,"fileExtension":"pdf","id":4,"locale":"TRE 1905198","featureId":"TRE 1905198"}
-    shownKeys.push((
-        <tr>
-            <td colSpan="2">{ getFileLinksForFeature(file._$layerId, file._$files) }</td>
-        </tr>));
-
     Messaging.open({
         title: 'Ladattava kohde',
-        content: (<table><tbody>{shownKeys}</tbody></table>)
+        content: (<table><tbody>{getFeatureElement(file)}</tbody></table>)
     });
 };
 
-function getFileLinksForFeature (layerId, files) {
-    var url = Oskari.urls.getRoute('WFSAttachments') + `&layerId=${layerId}`;
-    return files.map(f => {
-        let fileLink = `${url}&fileId=${f.id}`;
-        if (f.external) {
-            const fileName = encodeURIComponent(f.locale) + '.' + f.fileExtension;
-            fileLink = `&featureId=${f.featureId}&name=${fileName}`;
-        }
-        return (<a key={fileLink} className="button" rel="noopener noreferer" target="_blank" href={fileLink}>{f.locale}</a>);
-    });
-}
-
 const selectedFiles = {};
+const listeners = [];
+const notify = () => {
+    listeners.forEach(l => l());
+};
+
 export const Basket = {
     add: (file) => {
         selectedFiles[getFileId(file)] = file;
-        showMessage(file);
+        notify();
     },
     list: () => Object.values(selectedFiles),
-    remove: (file) => { delete selectedFiles[getFileId(file)]; }
+    remove: (file) => {
+        delete selectedFiles[getFileId(file)];
+        notify();
+    },
+    onChange: (listener) => listeners.push(listener)
 };
-/*
-var url = Oskari.urls.getRoute('WFSAttachments') + `&layerId=${layerId}`;
-const html = files.map(f => {
-    let fileLink = `&fileId=${f.id}`;
-    if (f.external) {
-        const fileName = encodeURIComponent(f.locale) + '.' + f.fileExtension;
-        fileLink = `&featureId=${f.featureId}&name=${fileName}`;
-    }
-    return `<a class="button" target="_blank" 
-        rel="noopener noreferer" href="${url + fileLink}">${f.locale}</a>`;
-});
-*/
