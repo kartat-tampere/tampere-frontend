61140c4e4eccf6261149a25431f5bb2fc2a620f1
Sami Mäkinen <zakarfin@gmail.com>
2020-05-21 23:37:40 +0300

    Add Popup component for GFI'ish behavior

---
+++ bundles/file-layerlist/components/Popup.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Overlay from 'ol/Overlay';
+import './Popup.css';
+import { getFeatureElement } from '../featureshelper';
+
+let overlay;
+
+export const hidePopup = () => {
+    overlay && overlay.setPosition(undefined);
+    const closer = document.getElementById('popup-closer');
+    closer && closer.blur();
+    return false;
+};
+
+export const addMapOverlay = () => {
+    const wrapper = document.createElement('div');
+    document.body.appendChild(wrapper);
+    ReactDOM.render(<div id="popup" className="ol-popup">
+        <a href="#" id="popup-closer" className="ol-popup-closer"></a>
+        <div id="popup-content"></div>
+    </div>, wrapper);
+
+    overlay = new Overlay({
+        element: document.getElementById('popup'),
+        autoPan: true,
+        autoPanAnimation: {
+            duration: 250
+        }
+    });
+
+    const olMap = Oskari.getSandbox().findRegisteredModuleInstance('MainMapModule').getMap();
+    olMap.addOverlay(overlay);
+};
+
+export const showPopup = (x, y, content) => {
+    if (!overlay) {
+        addMapOverlay();
+    }
+    const el = document.getElementById('popup-content');
+    ReactDOM.render(<React.Fragment>{getFeatureElement(content, true)}</React.Fragment>, el);
+    overlay.setPosition([x, y]);
+    const closeBtn = document.getElementById('popup-closer');
+    if (!closeBtn.onclick) {
+        closeBtn.onclick = hidePopup;
+    }
+};
