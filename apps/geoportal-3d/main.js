ca3008b3caf43b1b28a309a4ef171a4c2df26e50
Sami Mäkinen <zakarfin@gmail.com>
2020-09-06 22:51:43 +0300

    Initial 3D app

---
+++ apps/geoportal-3d/main.js
@@ -0,0 +1,68 @@
+import 'oskari-loader!../../bundles/lang-overrides/bundle.js';
+
+// These framework bundles have to be imported first
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/mapfull/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/oskariui/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/ui-components/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/divmanazer/bundle.js';
+
+// 3D mapmodule
+import 'oskari-loader!oskari-frontend/packages/mapping/olcs/mapmodule/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/olcs/map3dtiles/bundle.js';
+
+import 'oskari-loader!oskari-frontend/packages/mapping/time-control-3d/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/camera-controls-3d/bundle.js';
+// import 'oskari-loader!oskari-frontend/packages/mapping/dimension-change/bundle.js';
+
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapwmts/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/wfsvector/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapmyplaces/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/mapuserlayers/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/maparcgis/bundle.js';
+
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/drawtools/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/toolbar/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/statehandler/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/infobox/bundle.js';
+
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/backendstatus/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/search/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/hierarchical-layerlist/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/personaldata/bundle.js';
+// import 'oskari-loader!oskari-frontend/packages/framework/bundle/publisher2/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/maplegend/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/userguide/bundle.js';
+
+import 'oskari-loader!oskari-frontend/packages/catalogue/metadatacatalogue/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/catalogue/bundle/metadataflyout/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/featuredata2/bundle.js';
+
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/myplaces3/bundle.js';
+
+//import 'oskari-loader!oskari-frontend/packages/framework/bundle/guidedtour/bundle.js';
+//import 'oskari-loader!oskari-frontend/packages/framework/bundle/printout/bundle.js';
+
+//import 'oskari-loader!oskari-frontend/packages/framework/bundle/myplacesimport/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/coordinatetool/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/mapping/ol/maprotator/bundle.js';
+import 'oskari-loader!oskari-frontend/packages/framework/bundle/timeseries/bundle.js';
+
+// tampereeeeee
+import 'oskari-loader!oskari-frontend-contrib/packages/tampere/bundle/search-from-channels/bundle.js';
+import 'oskari-loader!oskari-frontend-contrib/packages/tampere/bundle/selected-featuredata/bundle.js';
+// import 'oskari-lazy-loader?content-editor!oskari-frontend-contrib/packages/tampere/bundle/content-editor/bundle.js';
+
+// import 'oskari-loader!../../bundles/file-upload/bundle.js';
+
+// import 'oskari-lazy-loader?file-layerlist!../../bundles/file-layerlist/bundle.js';
+
+// /tampereeeeee
+
+import 'oskari-lazy-loader?admin-hierarchical-layerlist!oskari-frontend/packages/integration/bundle/bb/bundle.js';
+import 'oskari-lazy-loader?admin-hierarchical-layerlist!oskari-frontend/packages/admin/bundle/admin-hierarchical-layerlist/bundle.js';
+import 'oskari-lazy-loader?admin-layerrights!oskari-frontend/packages/framework/bundle/admin-layerrights/bundle.js';
+import 'oskari-lazy-loader?admin-users!oskari-frontend/packages/framework/bundle/admin-users/bundle.js';
+import 'oskari-lazy-loader?admin!oskari-frontend/packages/admin/bundle/admin/bundle.js';
+import 'oskari-lazy-loader?admin-wfs-search-channel!oskari-frontend-contrib/packages/tampere/bundle/admin-wfs-search-channel/bundle.js';
+
+import './css/overwritten.css';
