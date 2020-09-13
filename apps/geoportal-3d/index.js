846c245e31b979a92ffe240a0e722759657dd712
Sami Mäkinen <zakarfin@gmail.com>
2020-09-13 23:43:16 +0300

    Fix appsetup path

--- apps/geoportal-3d/index.js
+++ apps/geoportal-3d/index.js
@@ -29,7 +29,7 @@
 })(window);
 
 jQuery(document).ready(function () {
-    Oskari.app.loadAppSetup(window.location.pathname + 'action_route=GetAppSetup', window.controlParams, function () {
+    Oskari.app.loadAppSetup(window.location.pathname + 'action?action_route=GetAppSetup', window.controlParams, function () {
         jQuery('#mapdiv').append('Unable to start');
     });
 });
